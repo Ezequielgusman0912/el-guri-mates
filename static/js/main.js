a94945e488ba2e761d7c3cfb9f5bdc64672076b8
Ezequielgusman0912 <egusman@portahnos.com.ar>
2026-09-03 12:22:34 -0300

    resposive

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const menuToggle = document.querySelector('.menu-toggle');
+  const mainNav = document.querySelector('.main-nav');
+
+  menuToggle?.addEventListener('click', () => {
+    const isOpen = mainNav.classList.toggle('is-open');
+    menuToggle.setAttribute('aria-expanded', String(isOpen));
+  });
+
   const messages = document.querySelectorAll('.message');
   messages.forEach((el) => {
     setTimeout(() => {
